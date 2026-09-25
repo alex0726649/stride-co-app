a38fc3f7392450dad0ec7bcd042153abecc0edf1
alex0726649 <alexperezmillan26@gmail.com>
2026-09-25 10:55:43 -0600

    chore: add deployment configuration

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const controller = require('../controllers/index');
 
+router.get('/health', controller.healthCheck);
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Stride & Co' });
